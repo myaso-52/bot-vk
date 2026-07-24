b253bf5d5e63d6fc8c37f42eabfd89b8e379c639
myaso-52 <Lebedev.aleks12@gmail.com>
2026-07-24 04:12:04 +0300

    Update db.py

--- src/db.py
+++ src/db.py
@@ -22,7 +22,8 @@ def init_db():
             nickname TEXT DEFAULT 'Игрок',
             no_cd_until REAL DEFAULT 0.0,
             x2_until INTEGER DEFAULT 0,
-            reg_date TEXT DEFAULT ''
+            reg_date TEXT DEFAULT '',
+            has_legendary INTEGER DEFAULT 0
         )
     ''')
     cursor.execute('''
@@ -48,8 +49,8 @@ def get_user(user_id):
             INSERT INTO users (
                 user_id, balance, moder_rank, clicks_count, last_click, 
                 last_daily, total_withdrawn, is_perm_banned, ban_until, 
-                ban_reason, nickname, no_cd_until, x2_until, reg_date
-            ) VALUES (?, 0, 0, 0, 0.0, 0.0, 0, 0, 0.0, '', 'Игрок', 0.0, 0, ?)
+                ban_reason, nickname, no_cd_until, x2_until, reg_date, has_legendary
+            ) VALUES (?, 0, 0, 0, 0.0, 0.0, 0, 0, 0.0, '', 'Игрок', 0.0, 0, ?, 0)
         ''', (user_id, current_date))
         conn.commit()
         cursor.execute("SELECT * FROM users WHERE user_id = ?", (user_id,))
@@ -61,7 +62,7 @@ def update_user_field(user_id, field_name, value):
     allowed_fields = [
         'balance', 'moder_rank', 'clicks_count', 'last_click', 
         'last_daily', 'total_withdrawn', 'is_perm_banned', 
-        'ban_until', 'ban_reason', 'nickname', 'no_cd_until', 'x2_until', 'reg_date'
+        'ban_until', 'ban_reason', 'nickname', 'no_cd_until', 'x2_until', 'reg_date', 'has_legendary'
     ]
     if field_name not in allowed_fields: return
     conn = sqlite3.connect(DB_PATH)
@@ -78,7 +79,7 @@ def add_balance(user_id, amount):
     cursor.execute("SELECT balance FROM users WHERE user_id = ?", (user_id,))
     new_balance = cursor.fetchone()
     conn.close()
-    return new_balance
+    return new_balance[0] if new_balance else 0
 
 def add_withdraw_log(user_id, amount):
     conn = sqlite3.connect(DB_PATH)
@@ -86,3 +87,12 @@ def add_withdraw_log(user_id, amount):
     cursor.execute("INSERT INTO withdraw_logs (user_id, amount, timestamp) VALUES (?, ?, ?)", (user_id, amount, time.time()))
     conn.commit()
     conn.close()
+
+def get_last_logs(limit=10):
+    conn = sqlite3.connect(DB_PATH)
+    conn.row_factory = sqlite3.Row
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM withdraw_logs ORDER BY id DESC LIMIT ?", (limit,))
+    rows = cursor.fetchall()
+    conn.close()
+    return [dict(r) for r in rows]
